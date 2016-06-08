d2d861280236bd1cce5ec78e2debfd4ea3060158
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-07 23:28:25 -0600

    added SMACSS.json property order and CSS sort config to gulpfile. Added gulp tasks to edit down from alternative sources

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,55 @@
 'use strict';
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    cssSortConfig = require('./smacss.json')
 
+//==========================================
+// SASS preprocessing
+//==========================================
+// Sorts properties form SCSS files
+gulp.task('sort-scss', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(gulpPostcss([
+      gulpPostcssSort({
+        "empty-lines-between-children-rules": 1,
+        "sort-order": cssSortConfig
+      })
+    ]))
+    .pipe(gulp.dest('app/sass/'));
+});
+// Compile documentation site SASS
+gulp.task('sass-patterns-clean', ['build-eds'], function() {
+    return gulp.src('app/patterns/**/*.css*').pipe(clean());
+});
+
+gulp.task('sass-patterns-clean-no-build', function() {
+    return gulp.src('app/patterns/**/*.css*').pipe(clean());
+});
+
+gulp.task('sass-patterns', ['sass-patterns-clean'], function() {
+    return gulp.src(['app/patterns/**/*.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            importer: importOnce,
+            importOnce: {
+                index: false,
+                css: false,
+                bower: false
+            }
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions', 'not ie <= 9'],
+            cascade: true
+        }))
+        .pipe(sourcemaps.write({
+            addComment: false
+        }))
+        .pipe(gulp.dest('app/patterns'))
+});
+
+//==========================================
 // PHP Server
+//==========================================
 gulp.task('connect-sync', function() {
   connect.server({}, function (){
     browserSync({
