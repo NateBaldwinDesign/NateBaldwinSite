4f2a0e2cb8f02738822b3b9859fa6334c7b3ab01
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-15 12:06:04 -0600

    updated colors and homepage. Added more typography and social icons

---
+++ parts/shared/social-links.php
@@ -0,0 +1,32 @@
+<section class="social">
+	<a class="social__item" target="blank" href="#">
+		<span class="social__icon">
+			<?php 
+				echo file_get_contents('wp-content/themes/NateBaldwinSite/img/social-twitter.svg');
+			?>
+		</span>
+		<span class="social__label">
+			@NateBaldwinArt
+		</span>
+	</a>
+	<a class="social__item" target="blank" href="#">
+		<span class="social__icon">
+			<?php 
+				echo file_get_contents('wp-content/themes/NateBaldwinSite/img/social-linkedin.svg');
+			?>
+		</span>
+		<span class="social__label">
+			Linkedin
+		</span>
+	</a>
+	<a class="social__item" target="blank" href="#">
+		<span class="social__icon">
+			<?php 
+				echo file_get_contents('wp-content/themes/NateBaldwinSite/img/social-medium.svg');
+			?>
+		</span>
+		<span class="social__label">
+			Medium
+		</span>
+	</a>
+</section>
