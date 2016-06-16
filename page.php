7fdc55face8b690656cb72c727b91e024cc6390c
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-16 14:02:40 -0600

    reverted homepage back to default content. Updated red color. Tweaked social icons to be spaced using baseline.

--- page.php
+++ page.php
@@ -1,37 +1,11 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
 
-<hgroup>
-	<h1>Heading</h1>
-	<h2>Subheading</h2>
-</hgroup>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
-</p>
-<blockquote>This is a quote that I wrote on a note while I slept on a boat.</blockquote>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
-</p>
-<hr/>
-<hgroup>
-	<h1 class="headline">Nate Baldwin</h1>
-	<h2 class="lead">Subheading lead sentence or description copy that's really long about what I do.</h2>
-</hgroup>
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
-</p>
-
-
-
-<!-- ACTUAL HOME PAGE ---->
-
-<!-- <div id="logo-accent">
-	<?php echo file_get_contents('img/NB_head-logo.svg') ?>
-</div>
-
 <div class="l-section--hero">
 	<div class="l-container--fluid">
-		<h1>Nate Baldwin</h1>
-		<span class="lead"><?php the_content(); ?></span>
+		<hgroup>
+			<h1 class="heading">Nate Baldwin</h1>
+			<h2 class="lead">User Experience Designer, Illustrator, Artist <span class="fancy-amp">&amp;</span> Front-end Developer</h2>
+		</hgroup>
 	</div>
 </div>
 
@@ -42,4 +16,3 @@
 		</div>
 	</div>
 </div>
- -->
