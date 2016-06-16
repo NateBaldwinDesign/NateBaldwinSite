aee49aa11575907206a2325dbc1af5f3335128e7
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-16 11:34:52 -0600

    removed sassline. Added typesettings and a gist for displaying a grid overlay. Added core typesettings scss into repo for customization.

--- page.php
+++ page.php
@@ -1,8 +1,39 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
-<!-- <div class="l-section--primary" id="header-triangle">
-	<?php echo file_get_contents('img/section-angle.svg') ?>
-</div> -->
-<div id="logo-accent">
+
+<h1>Heading</h1>
+<h2>Heading</h2>
+<h3>Heading</h3>
+<h4>Heading</h4>
+<h5>Heading</h5>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
+</p>
+<p>
+	Donec convallis efficitur sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec tincidunt ante sit amet nulla lobortis pellentesque. Proin semper nisi commodo, pretium tortor vitae, accumsan elit. Pellentesque at convallis mi. Ut blandit ligula id orci dapibus rutrum. Cras ut quam quis dui porta bibendum. Curabitur augue risus, aliquet at ullamcorper in, facilisis ac ipsum. Nulla pellentesque, dolor eget efficitur facilisis, ligula massa commodo diam, eget sollicitudin lacus odio id nunc. Vivamus ornare eleifend sem. Proin bibendum elit risus, non commodo nisl viverra bibendum. Nulla posuere tincidunt risus, at efficitur mi faucibus eget.
+</p>
+<blockquote>This is a quote that I wrote on a note while I slept on a boat.</blockquote>
+<hr/>
+<hgroup>
+	<h1>Heading</h1>
+	<h2>Subheading</h2>
+</hgroup>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
+</p>
+<hr/>
+<hgroup>
+	<h2>Heading</h2>
+	<h3>Subheading</h3>
+</hgroup>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec orci eros, tincidunt ut fringilla sit amet, vestibulum id nisl. In suscipit malesuada felis, vel iaculis diam vulputate eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam erat volutpat. Ut eu augue at ante vestibulum fringilla. Sed vel sapien id elit vulputate dictum non sit amet nibh. Cras eget dolor tempus, imperdiet nibh in, pretium lectus. Aenean tristique sollicitudin tincidunt. Ut ut vehicula leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed ut pharetra risus.
+</p>
+
+
+
+<!-- ACTUAL HOME PAGE ---->
+
+<!-- <div id="logo-accent">
 	<?php echo file_get_contents('img/NB_head-logo.svg') ?>
 </div>
 
@@ -20,3 +51,4 @@
 		</div>
 	</div>
 </div>
+ -->
