5b230bc33c24b526b03725732d3800b864b79893
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-06-29 20:31:25 -0600

    made page.php the homepage again. Page templates not working currently.

--- page.php
+++ page.php
@@ -1,20 +1,24 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
 
-<div class="l-section--primary l-section--pageheader">
+<div class="l-section--hero">
 	<div class="l-container--fluid u-flex-center">
 		<hgroup>
-			<h1 class="heading"><?php the_title(); ?></h1>
+			<span class="hero-logo__wrapper">
+				<svg class="hero-logo" ariaLabelledby="title">
+					<title>Nate Baldwin</title>
+					<use xlink:href="#icon-NB_logo_type"></use>
+				</svg>
+			</span>
+			<!-- <h1 class="heading">Nate Baldwin</h1> -->
+			<h2 class="lead">User Experience Designer, Illustrator, Artist &amp; Front-end Developer</h2>
 		</hgroup>
 	</div>
 </div>
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<div class="l-section--white">
+
+<div class="l-section--primary">
 	<div class="l-section__content">
 		<div class="l-container--fluid l-container--center">
-			<?php the_content(); ?>
+			<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/social-links','parts/shared/footer-portrait','parts/shared/footer','parts/shared/html-footer' ) ); ?>
 		</div>
 	</div>
 </div>
-<?php endwhile; ?>
-
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
