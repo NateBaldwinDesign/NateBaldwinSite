fc17c629ed9f18c91ecf78e0a4c971a91c5c6200
Nathan Baldwin <nbaldwin@USD1500418.local>
2016-07-02 02:12:38 -0600

    added back button to portfolio case study pages. Changed primary color to muted green. Updated font-weights and added link text styles.

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -1,7 +1,7 @@
 <header>
-	<a class="header__logo" href="<?php echo get_site_url(); ?>">
-		<svg>
-			<use xlink:href="#icon-NB_head-logo"></use>
+	<a class="icon__container--circle header__back-button" href="<?php echo get_site_url(); ?>">
+		<svg class="icon">
+			<use xlink:href="#icon-arrow-left"></use>
 		</svg>
 	</a>
 <!-- 	<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'class' => 'header__nav' ) ); ?>-->
